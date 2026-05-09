0bb42b5482960c83d85a0cf4909a83956fe49ca7
h2o-lerma-con-encanto <colectivo.h2olerma@hotmail.com>
2026-05-09 15:03:37 -0600

    feat(auth): Se agregó autenticación por token API de autenticación y registro de cuentas.
    // POST /api/auth/register
    // POST /api/auth/login

--- src/server.js
+++ src/server.js
@@ -16,16 +16,18 @@ app.use(express.json());
 connectDB();
 
 // Rutas existentes
+app.use('/api/auth', require('./routes/authRoutes'));
 
 // Servir archivos estáticos de client/
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-// Ruta raíz -> main page
+// Ruta raíz -> main page index
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
 
-
+// Si se ejecuta en modo de desarrollo o producción, manda mensaje de confirmación
+// y ejecuta en el puerto asignado
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
