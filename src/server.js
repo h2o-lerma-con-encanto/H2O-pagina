fc36a0d700e7a1eb88914c983a1df5af1bc210f0
h2o-lerma-con-encanto <colectivo.h2olerma@hotmail.com>
2026-05-09 14:58:09 -0600

    Revert "feat(auth)"
    This reverts commit 9cbca7e196aa1963c8f673daff7e2c0e0f72326f.

--- src/server.js
+++ src/server.js
@@ -16,18 +16,16 @@ app.use(express.json());
 connectDB();
 
 // Rutas existentes
-app.use('/api/auth', require('./routes/authRoutes'));
 
 // Servir archivos estáticos de client/
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-// Ruta raíz -> main page index
+// Ruta raíz -> main page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 });
 
-// Si se ejecuta en modo de desarrollo o producción, manda mensaje de confirmación
-// y ejecuta en el puerto asignado
+
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
