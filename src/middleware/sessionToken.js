c6ed74e5dbabc25353557b8f7b2a653199648e1d
h2o-lerma-con-encanto <colectivo.h2olerma@hotmail.com>
2026-06-04 12:44:56 -0600

    fix(middleware): Nombres y datos en sessionToken - El nombre del parámetro cambió de 'user' a 'usuario'. - El token ya no se genera con el nombre, ahora solicita el user

--- src/middleware/sessionToken.js
+++ src/middleware/sessionToken.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 // Helper: generar token con id, user y rol del Usuario
 const generateToken = (usuario) => {
   const payload = {
-    id: user._id,
-    user: user.user,
-    role: user.rol
+    id: usuario._id,
+    user: usuario.user,
+    role: usuario.rol
   };
   const expiresIn = process.env.TOKEN_EXPIRES_IN;
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
